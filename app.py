df21286c1d5e1c912c722b2b1e65af7f97bc9da5
Greg Kopels <gkopels@cisco.com>
2020-12-12 14:56:25 +0200

    c

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import flask
+app = Flask(_name_)
+
+@app.route("/")
+
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+
+def hello():
+    return 'I am good, how are you?'
+
+if __name__ == "__name__":
+    app.run(host="0.0.0.0", port=8080)
